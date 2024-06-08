9f39128983843acb343f2f8738e682a8383a2448
UdayaBharagvi <suravarapuudayabhargavi@gmail.com>
2024-06-08 10:43:00 +0530

    Add files via upload
    Task

---
+++ Task1.py
@@ -0,0 +1,39 @@
+# Create a list
+my_list = [1, 2, 3, 4,5]
+
+# Add an element to the list
+my_list.append(6)
+
+# Remove an element from the list
+my_list.remove(3)
+
+# Modify an element in the list
+my_list[0] = 7
+print("List:", my_list)
+
+
+# Creating a dictionary
+my_dict = {'a': 1, 'b': 2,  'c': 3}
+
+#Add a key-value pair to the dictionary
+my_dict['s'] = 4
+
+#Removing a key- value pair from the dictionary
+del my_dict['b']
+
+#Modifying a value in the dictionary
+my_dict['a'] = 100
+
+print("Dictionary:", my_dict)
+
+# Creating a set
+my_set = {1, 2, 3, 4, 5}
+
+#Adding an element to the set
+my_set.add(6)
+
+#Removing an element from the set
+my_set.remove(3)
+
+
+print("Set:", my_set)
